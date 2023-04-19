b9589fadad817ec97a5863a07936478249c731b5
yogotaku <takuya.yogo@divx.co.jp>
2023-04-19 21:19:43 +0900

    variables

---
+++ variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let y = [0, 1, 2, 3];
+    println!("The value of y is: {}", y[3]);
+}
